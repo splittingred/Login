49ef77333fbc28c4fe6fd47cacf50576464fbb5a
Shaun McCormick <shaun@modxcms.com>
2010-07-05 10:33:14 -0500

    Added partial french translation

---
+++ core/components/login/lexicon/fr/register.inc.php
@@ -0,0 +1,50 @@
+<?php
+/**
+ * Login
+ *
+ * Copyright 2010 by Jason Coward <jason@modxcms.com> and Shaun McCormick
+ * <shaun@modxcms.com>
+ *
+ * Login is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License as published by the Free
+ * Software Foundation; either version 2 of the License, or (at your option) any
+ * later version.
+ *
+ * Login is distributed in the hope that it will be useful, but WITHOUT ANY
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
+ * A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along with
+ * Login; if not, write to the Free Software Foundation, Inc., 59 Temple
+ * Place, Suite 330, Boston, MA 02111-1307 USA
+ *
+ * @package login
+ */
+/**
+ * Register French lexicon for Login
+ *
+ * @package login
+ * @subpackage lexicon
+ */
+$_lang['register.email'] = 'Adresse électronique';
+$_lang['register.email_invalid'] = 'Veuillez entrer une adresse électronique valide.';
+$_lang['register.email_invalid_domain'] = 'Nom de domaine de votre adresse électronique invalide.';
+$_lang['register.field_not_empty'] = 'Ce champ doit être vide.';
+$_lang['register.field_required'] = 'Ce champ n\'est pas renseigné.';
+$_lang['register.fullname'] = 'Nom complet';
+$_lang['register.max_length'] = 'Maximum [[+length]] caractères.';
+$_lang['register.max_value'] = 'Valeur maximum : [[+value]].';
+$_lang['register.min_length'] = 'Minimum : [[+length]] caractères.';
+$_lang['register.min_value'] = 'Valeur minimum : [[+value]].';
+$_lang['register.not_date'] = 'Date invalide.';
+$_lang['register.not_number'] = 'Valeur invalide.';
+$_lang['register.password'] = 'Mot de passe';
+$_lang['register.password_confirm'] = 'Confirmer le mot de passe';
+$_lang['register.password_dont_match'] = 'Les mots de passe ne concordent pas.';
+$_lang['register.password_not_confirmed'] = 'Veuillez confirmer votre mot de passe.';
+$_lang['register.range_invalid'] = 'Intervalle invalide.';
+$_lang['register.range'] = 'Cette valeur doit être comprise entre [[+min]] et [[+max]].';
+$_lang['register.register'] = 'Inscription';
+$_lang['register.user_err_save'] = 'Une erreur est survenue.';
+$_lang['register.username'] = 'Identifiant';
+$_lang['register.username_taken'] = 'Identifiant déjà utilisé par un autre membre.';
---
+++ core/components/login/lexicon/fr/updateprofile.inc.php
@@ -0,0 +1,43 @@
+<?php
+/**
+ * Login
+ *
+ * Copyright 2010 by Jason Coward <jason@modxcms.com> and Shaun McCormick
+ * <shaun@modxcms.com>
+ *
+ * Login is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License as published by the Free
+ * Software Foundation; either version 2 of the License, or (at your option) any
+ * later version.
+ *
+ * Login is distributed in the hope that it will be useful, but WITHOUT ANY
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
+ * A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along with
+ * Login; if not, write to the Free Software Foundation, Inc., 59 Temple
+ * Place, Suite 330, Boston, MA 02111-1307 USA
+ *
+ * @package login
+ */
+/**
+ * Update Profile French lexicon for Login
+ *
+ * @package login
+ * @subpackage lexicon
+ */
+$_lang['login.address'] = 'Adresse';
+$_lang['login.city'] = 'Ville';
+$_lang['login.country'] = 'Pays';
+$_lang['login.email'] = 'Adresse électronique';
+$_lang['login.fax'] = 'Fax';
+$_lang['login.fullname'] = 'Nom complet';
+$_lang['login.mobilephone'] = 'Téléphone mobile';
+$_lang['login.phone'] = 'Téléphone';
+$_lang['login.profile_err_nf'] = 'Pofil non trouvé!';
+$_lang['login.profile_err_save'] = 'Une erreur est survenue durant la mise à jour du profil.';
+$_lang['login.profile_updated'] = 'Profil mis à jour.';
+$_lang['login.state'] = 'État';
+$_lang['login.update_profile'] = 'Mettre le profil à jour';
+$_lang['login.website'] = 'Site web';
+$_lang['login.zip'] = 'Code postal';
