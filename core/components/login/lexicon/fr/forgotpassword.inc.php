cffc5d002b6993da4211c7f3d96f01f06548a6fe
Pierre-Luc Auclair <p-l.auclair@velourbain.qc.ca>
2010-07-25 02:12:27 +0800

    Finished french translation, updated some existing strings to match with Revo naming scheme

---
+++ core/components/login/lexicon/fr/forgotpassword.inc.php
@@ -0,0 +1,36 @@
+<?php
+/**
+ * Login
+ *
+ * Copyright 2010 by Jason Coward <jason@modxcms.com> and Shaun McCormick
+ * <shaun@modxcms.com>
+ *
+ * Login is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License as published by the Free
+ * Software Foundation; either version 2 of the License, or (at your option) any
+ * later version.
+ *
+ * Login is distributed in the hope that it will be useful, but WITHOUT ANY
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
+ * A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along with
+ * Login; if not, write to the Free Software Foundation, Inc., 59 Temple
+ * Place, Suite 330, Boston, MA 02111-1307 USA
+ *
+ * @package login
+ */
+/**
+ * Forgot Password English lexicon for Login
+ *
+ * @package login
+ * @subpackage lexicon
+ */
+$_lang['login.email'] = 'Adresse email';
+$_lang['login.forgot_password'] = 'Mot de passe oublié';
+$_lang['login.forgot_password_email_subject'] = 'Email de récupération de mot de passe';
+$_lang['login.or_forgot_username'] = 'Oublié votre identifiant ?';
+$_lang['login.reset_password'] = 'Réinitialiser le mot de passe';
+$_lang['login.username'] = 'Identifiant';
+$_lang['login.user_err_nf_email'] = 'Aucun utilisateur n\'a été trouvé avec cette adresse email.';
+$_lang['login.user_err_nf_username'] = 'Aucun utilisateur n\'a été trouvé avec cet identifiant.';
