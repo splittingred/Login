69e12612b2512f03a7cd50b1028ddda671679f0f
Romain <romain@meltingmedia.net>
2011-01-29 19:13:21 +0100

    preparing french translation

---
+++ core/components/login/lexicon/fr/recaptcha.inc.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * reCaptcha Lexicon Topic
+ *
+ * @package login
+ * @subpackge lexicon
+ */
+$_lang['recaptcha.empty_answer'] = 'Incorrect captcha words. Please check your answer and try again.';
+$_lang['recaptcha.incorrect'] = 'The reCAPTCHA wasn\'t entered correctly. Go back and try it again. [[+error]]';
+$_lang['recaptcha.mailhide_no_mcrypt'] = 'To use reCAPTCHA Mailhide, you need to have the mcrypt php module installed.';
+$_lang['recaptcha.mailhide_no_api_key'] = 'To use reCAPTCHA Mailhide, you have to sign up for a public and private key, you can do so at <a href="http://mailhide.recaptcha.net/apikey">http://mailhide.recaptcha.net/apikey</a>';
+$_lang['recaptcha.no_api_key'] = 'To use reCAPTCHA you must get an API key from <a href="http://recaptcha.net/api/getkey">http://recaptcha.net/api/getkey</a>';
+$_lang['recaptcha.no_remote_ip'] = 'For security reasons, you must pass the remote ip to reCAPTCHA';
+
