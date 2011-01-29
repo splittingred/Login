4c43d92dcf739b15473e8dcd1c51316d1f6b696f
Romain <romain@meltingmedia.net>
2011-01-29 19:29:49 +0100

    recaptcha.inc.php

--- core/components/login/lexicon/fr/recaptcha.inc.php
+++ core/components/login/lexicon/fr/recaptcha.inc.php
@@ -5,10 +5,10 @@
  * @package login
  * @subpackge lexicon
  */
-$_lang['recaptcha.empty_answer'] = 'Incorrect captcha words. Please check your answer and try again.';
-$_lang['recaptcha.incorrect'] = 'The reCAPTCHA wasn\'t entered correctly. Go back and try it again. [[+error]]';
-$_lang['recaptcha.mailhide_no_mcrypt'] = 'To use reCAPTCHA Mailhide, you need to have the mcrypt php module installed.';
-$_lang['recaptcha.mailhide_no_api_key'] = 'To use reCAPTCHA Mailhide, you have to sign up for a public and private key, you can do so at <a href="http://mailhide.recaptcha.net/apikey">http://mailhide.recaptcha.net/apikey</a>';
-$_lang['recaptcha.no_api_key'] = 'To use reCAPTCHA you must get an API key from <a href="http://recaptcha.net/api/getkey">http://recaptcha.net/api/getkey</a>';
-$_lang['recaptcha.no_remote_ip'] = 'For security reasons, you must pass the remote ip to reCAPTCHA';
+$_lang['recaptcha.empty_answer'] = 'Réponse captcha incorrecte. Veuillez vérifier votre réponse et essayer à nouveau.';
+$_lang['recaptcha.incorrect'] = 'Le reCAPTCHA n\'a pas été entré correctement. veuillez réessayer. [[+error]]';
+$_lang['recaptcha.mailhide_no_mcrypt'] = 'Pour utiliser reCAPTCHA Mailhide, vous avez besoin du module PHP mcrypt.';
+$_lang['recaptcha.mailhide_no_api_key'] = 'Pour utiliser reCAPTCHA Mailhide, vous devez vous enregistrer sur <a href="http://mailhide.recaptcha.net/apikey">http://mailhide.recaptcha.net/apikey</a> pour obtenir une clé publique et une privée';
+$_lang['recaptcha.no_api_key'] = 'Pour utiliser reCAPTCHA vous devez utiliser une clé d\'API fournie sur <a href="http://recaptcha.net/api/getkey">http://recaptcha.net/api/getkey</a>';
+$_lang['recaptcha.no_remote_ip'] = 'Pour des raisons de sécurité, vous devez « passer » l\'adresse distante à reCAPTCHA';
 
