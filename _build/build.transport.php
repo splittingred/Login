d8fb0922921700aef589eb7f788a3b58640ccbaf
Shaun McCormick <shaun@modx.com>
2012-08-24 15:22:08 -0500

    Up build version

--- _build/build.transport.php
+++ _build/build.transport.php
@@ -35,7 +35,7 @@ set_time_limit(0);
 /* define version */
 define('PKG_NAME','Login');
 define('PKG_NAME_LOWER','login');
-define('PKG_VERSION','1.8.0');
+define('PKG_VERSION','1.8.1');
 define('PKG_RELEASE','pl');
 
 /* define sources */
